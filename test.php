7eb1b8e26419ef250f3fcc50673a49e578583d58
yanghongyu <1319112240@qq.com>
2019-09-17 13:47:09 +0800

    1.添加单例模式

--- test.php
+++ test.php
@@ -1,2 +1,18 @@
 <?php
 	header('Content-type:text.html;charset=utf8');
+
+	class User{
+		private static $_instance;
+		private function __construct(){
+
+		}
+		private function __clone(){
+
+		}
+		public static function getInstance(){
+			if(!(self::$_instance instanceof self)){
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+	}
